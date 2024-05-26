946c837b09703d2b3cee3a1e9c9bf81e47b30ad9
KUBIXQAZ <kubajozwik20027@gmail.com>
2024-05-26 20:59:34 +0200

    refresh command

---
+++ Commands/RefreshDataCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamItemsStatsViewer.Commands
+{
+    public class RefreshDataCommand : CommandBase
+    {
+        private Action _action;
+
+        public RefreshDataCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _action?.Invoke();
+        }
+    }
+}
--- ViewModels/DisplayItemDataViewModel.cs
+++ ViewModels/DisplayItemDataViewModel.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Windows.Input;
+using SteamItemsStatsViewer.Commands;
 
 namespace SteamItemsStatsViewer.ViewModels
 {
@@ -17,13 +19,18 @@ namespace SteamItemsStatsViewer.ViewModels
 
         public IEnumerable<DataGridItemModel> ItemsData => _itemsData;
 
+        public ICommand RefreshDataCommand { get; set; }
+
         public DisplayItemDataViewModel()
         {
             LoadData();
+
+            RefreshDataCommand = new RefreshDataCommand(LoadData);
         }
 
         private void LoadData()
         {
+            Debug.WriteLine("asdadadasasd");
             _itemsData.Clear();
 
             string dataFilePath = "D:\\PROGRAMMING-PROJECTS\\csharp-apps\\SteamMarketDataCollector\\bin\\Debug\\net8.0\\output\\Paris_2023_Legends_Sticker_Capsule.json";
