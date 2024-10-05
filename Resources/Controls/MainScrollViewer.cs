6670367a60d4c8fe21848774db1a280db193c68e
KUBIXQAZ <kubajozwik20027@gmail.com>
2024-10-05 23:20:30 +0200

    Now scroll is working on every page but not on DisplayItemData (i don't know how to solve issue with scrolling page and chart at the same time), switched from ListBox to ItemsControl

--- Resources/Controls/MainScrollViewer.cs
+++ Resources/Controls/MainScrollViewer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SteamItemsStatsViewer.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -10,9 +7,35 @@ namespace SteamItemsStatsViewer.Resources.Controls
 {
     public class MainScrollViewer : ScrollViewer
     {
+        public static readonly DependencyProperty ViewModelProperty =
+        DependencyProperty.Register(
+            "ViewModel",
+            typeof(ViewModelBase),
+            typeof(MainScrollViewer)
+        );
+
+        public ViewModelBase ViewModel
+        {
+            get 
+            { 
+                return (ViewModelBase)GetValue(ViewModelProperty); 
+            }
+            set 
+            { 
+                SetValue(ViewModelProperty, value); 
+            }
+        }
+
         protected override void OnMouseWheel(MouseWheelEventArgs e)
         {
-            return;
+            if(ViewModel is DisplayItemDataViewModel)
+            {
+                return;  
+            } 
+            else
+            {
+                base.OnMouseWheel(e);
+            }
         }
     }
 }
